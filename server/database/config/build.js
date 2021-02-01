6ab3b6e4e8c6d0d972e605f44d9d953259e705b9
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-01 15:17:15 +0200

    added database connection file plus the build function that reading the build.sql and fakedata.sql relates #53

---
+++ server/database/config/build.js
@@ -0,0 +1,9 @@
+const { readFileSync } = require('fs');
+const { join } = require('path');
+const connection = require('./connection');
+
+module.exports = () => {
+  const sqlSchema = readFileSync(join(__dirname, './build.sql')).toString();
+  const sqlFakeData = readFileSync(join(__dirname, './fakeData.sql')).toString();
+	return connection.query(sqlSchema + sqlFakeData);
+};
---
+++ server/database/config/connection.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+require('dotenv').config({ path: './config.env' });
+
+const {
+	DATABASE_URL,
+	TEST_DATABASE_URL,
+	PRODUCTION_DATABASE_URL,
+	NODE_ENV,
+} = process.env;
+
+let dbUrl = '';
+
+switch (NODE_ENV) {
+	case 'production':
+		dbUrl = PRODUCTION_DATABASE_URL;
+		break;
+	case 'development':
+		dbUrl = DATABASE_URL;
+		break;
+	case 'test':
+		dbUrl = TEST_DATABASE_URL;
+		break;
+	default:
+		throw new Error('No Database url!!!');
+}
+
+const options = {
+	connectionString: dbUrl,
+	ssl: NODE_ENV === 'production',
+};
+
+module.exports = new Pool(options);
