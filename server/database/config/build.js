51b95f44957731f74414caedef953d17737c3f9d
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-02 10:10:26 +0200

    edit spaces

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -3,9 +3,9 @@ const { join } = require('path');
 const connection = require('./connection');
 //Build Function
 module.exports = () => {
-	const sqlSchema = readFileSync(join(__dirname, './build.sql')).toString();
-	const sqlFakeData = readFileSync(
-		join(__dirname, './fakeData.sql')
-	).toString();
-	return connection.query(sqlSchema + sqlFakeData);
+ const sqlSchema = readFileSync(join(__dirname, './build.sql')).toString();
+ const sqlFakeData = readFileSync(
+  join(__dirname, './fakeData.sql')
+ ).toString();
+ return connection.query(sqlSchema + sqlFakeData);
 };
