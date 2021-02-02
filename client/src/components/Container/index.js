9317c1aec901ed23b9c97fb99975bf01a8cbeb77
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-03 00:49:20 +0200

    make container component with children and screensize props and make the defaultProps for screensize to be sm becuase of mobile first design relates #77

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Container } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
+const { node, string } = PropTypes;
+
 function ContainerComponent({ children, screenSize }) {
   return <Container maxWidth={screenSize}>{children}</Container>;
 }
 
 ContainerComponent.propTypes = {
-  children: PropTypes.node.isRequired,
-  screenSize: PropTypes.string.isRequired,
+  children: node.isRequired,
+  screenSize: string,
+};
+
+ContainerComponent.defaultProps = {
+  screenSize: 'sm',
 };
 
 export default ContainerComponent;
