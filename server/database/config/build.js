eb8d19b1848d8a13ee082ce88141602e31ac490f
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-02 10:02:02 +0200

    fix spaces in build.js

--- server/database/config/build.js
+++ server/database/config/build.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require('fs');
 const { join } = require('path');
 const connection = require('./connection');
-
 module.exports = () => {
 	const sqlSchema = readFileSync(join(__dirname, './build.sql')).toString();
 	const sqlFakeData = readFileSync(
