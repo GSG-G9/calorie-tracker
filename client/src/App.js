c1a30b32833bdf7e9b3663a6720c6c88cd90f3e1
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-02 12:34:56 +0200

    initial chart component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Chart from "./components/Chart"
 
 function App() {
   return (
     <div className="App">
       <h1> Hi from React App</h1>
+      <Chart />
     </div>
   );
 }
---
+++ client/src/components/Chart.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import { Doughnut } from 'react-chartjs-2';
+
+class Chart extends Component {
+    render() {
+        return (
+            <div className="chart">
+                {/* <Doughnut data={...} /> */}
+                CHART COMPONENT
+            </div>
+        )
+    }
+}
+
+
+export default Chart
