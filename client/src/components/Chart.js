8becf0f86cb83e6e50bc73f7e032e827d4e3a363
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-02 13:19:07 +0200

    bar chart experiment

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
-// import { Doughnut } from 'react-chartjs-2';
+import React, { Component } from "react";
+import { Doughnut } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
 
 class Chart extends Component {
-    render() {
-        return (
-            <div className="chart">
-                {/* <Doughnut data={...} /> */}
-                CHART COMPONENT
-            </div>
-        )
+    constructor(){
+        super(props);
+        this.state = {
+            chartData:{
+
+            }
+        }
     }
+  render() {
+    return (
+      <div className="chart">
+        {/* <Doughnut data={...} /> */}
+        CHART COMPONENT
+         <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Largest Cities In '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
 
-
-export default Chart
+export default Chart;
