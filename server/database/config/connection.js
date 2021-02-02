f20911c779149f79686ecdcb3d7490a336232ec6
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-02 09:49:41 +0200

    edit spaces

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,6 +1,5 @@
-const { Pool } = require('pg');
+const {} = require('pg');
 require('dotenv').config({ path: './config.env' });
-
 const {
 	DATABASE_URL,
 	TEST_DATABASE_URL,
@@ -8,9 +7,7 @@ const {
 	NODE_ENV,
 } = process.env;
 
-//Database Url
 let dbUrl = '';
-
 switch (NODE_ENV) {
 	case 'production':
 		dbUrl = PRODUCTION_DATABASE_URL;
@@ -24,7 +21,6 @@ switch (NODE_ENV) {
 	default:
 		throw new Error('No Database url!!!');
 }
-
 const options = {
 	connectionString: dbUrl,
 	ssl: NODE_ENV === 'production',
