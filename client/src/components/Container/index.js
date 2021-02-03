aaf6ec0ea80b638dfe69d59d5bbb6707382b785b
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-03 17:13:44 +0200

    add grid component to container and add children ,screensize,direction,itemColumns,spacing props relates #77

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 const { node, string } = PropTypes;
 
-function ContainerComponent({ children, screenSize }) {
-  return <Container maxWidth={screenSize}>{children}</Container>;
+function ContainerComponent({
+  children,
+  screenSize,
+  direction,
+  itemColumns,
+  spacing,
+  ...rest
+}) {
+  /*
+  <ContainerComponent
+      screenSize="sm"
+      direction="row"
+      gridWidth="3"
+      spacing="10"
+    >
+      <p>p1</p>
+      <p>p2</p>
+      <p>p3</p>
+      <p>p4</p>
+    </ContainerComponent> 
+  */
+  return (
+    <Container maxWidth={screenSize} {...rest}>
+      <Grid container direction={direction} justify="center" spacing={spacing}>
+        {children.map((element) => (
+          <Grid
+            item
+            sm={screenSize === 'sm' ? itemColumns : false}
+            md={screenSize === 'md' ? itemColumns : false}
+            lg={screenSize === 'lg' ? itemColumns : false}
+          >
+            {element}
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
 
 ContainerComponent.propTypes = {
   children: node.isRequired,
   screenSize: string,
+  direction: string.isRequired,
+  itemColumns: string.isRequired,
+  spacing: string.isRequired,
 };
 
 ContainerComponent.defaultProps = {
