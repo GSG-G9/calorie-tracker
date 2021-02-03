17119e576ddab98d061be029bcae1a4acd130405
zeinjendeya <z.jendeya@gmail.com>
2021-02-03 16:35:52 +0200

    recreated button

--- client/src/component/Button/index.js
+++ client/src/component/Button/index.js
@@ -4,42 +4,36 @@ import { Button } from '@material-ui/core';
 
 const IconLabeledButton = ({
   variant,
-  styles,
-  icon,
   children,
   event,
   disable,
-  starticon,
+  className,
+  ...rest
 }) => (
   <Button
     variant={variant}
-    style={styles}
-    endIcon={icon}
-    startIcon={starticon}
+    className={className}
     disabled={disable}
     onClick={event}
+    {...rest}
   >
     {children}
   </Button>
 );
 
-const { string, objectOf, element, func, bool, children } = PropTypes;
+const { string, func, bool, children } = PropTypes;
 
 IconLabeledButton.defaultProps = {
-  icon: null,
   event: null,
-  styles: { color: 'green' },
-  starticon: null,
+  className: null,
 };
 
 IconLabeledButton.propTypes = {
   variant: string.isRequired,
-  styles: objectOf(string),
-  icon: element,
+  className: string,
   children: children.isRequired,
   event: func,
   disable: bool.isRequired,
-  starticon: element,
 };
 
 export default IconLabeledButton;
