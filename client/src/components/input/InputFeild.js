7721b7f3115db404d27338588fa8a104590f1aa9
zeinjendeya <z.jendeya@gmail.com>
2021-02-03 17:28:09 +0200

    created textFeild component

---
+++ client/src/components/input/InputFeild.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextField } from '@material-ui/core';
+
+const TextInputField = ({
+  label,
+  className,
+  type,
+  variant,
+  placeholder,
+  onChange,
+  ...rest
+}) => (
+  <TextField
+    type={type}
+    className={className}
+    label={label}
+    variant={variant}
+    onChange={onChange}
+    placeholder={placeholder}
+    {...rest}
+  />
+);
+
+const { string, func } = PropTypes;
+
+TextInputField.propTypes = {
+  type: string.isRequired,
+  className: string.isRequired,
+  label: string.isRequired,
+  variant: string.isRequired,
+  onChange: func.isRequired,
+  placeholder: string.isRequired,
+};
+
+export default TextInputField;
