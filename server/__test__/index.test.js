6ad67557938ebf532999394c0a4b67b8e5d7e7df
zeinjendeya <z.jendeya@gmail.com>
2021-02-04 16:31:05 +0200

    deleted a test

--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -2,18 +2,6 @@ const supertest = require('supertest');
 const router = require('./../app');
 
 
-test('router returns 400 if the user does not exist', (done) => {
-  supertest(router)
-      .post("/api/v1/login")
-      .send({email: 'user@gmail.com', password: 'hohoho123123'})
-      .expect(400)
-      .end((err, res) => {
-        if (err) return done(err);
-        expect(res.statusCode).toBe(400);
-        done();
-      });
-});
-
 test('router returns 400 if there is no password', (done) => {
   supertest(router)
       .post("/login")
