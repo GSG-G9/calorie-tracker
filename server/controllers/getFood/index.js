ee37b98629e0e5883b16e296fce7cf6ea812a109
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-04 14:48:56 +0200

    fix spacing problem

--- server/controllers/getFood/index.js
+++ server/controllers/getFood/index.js
@@ -1,11 +1,11 @@
 const { getFood } = require('../../database/queries');
 
 module.exports = (req, res,next) => {
-	getFood().then(({ rows }) =>
-		res.status(200).json({
-			message: 'success',
-			status: 200,
-			data: rows,
-		})
-	).catch(next);
+  getFood().then(({ rows }) =>
+    res.status(200).json({
+      message: 'success',
+      status: 200,
+      data: rows,
+    })
+  ).catch(next);
 };
