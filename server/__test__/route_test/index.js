a475f1c798ad86288eecd6ee821db19210522bb2
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-05 03:58:46 +0200

    making query test

--- server/__test__/route_test/index.js
+++ server/__test__/route_test/index.js
@@ -1,28 +1,31 @@
-const request = require('supertest');
-const app = require('../../app');
+const request = require("supertest");
+const app = require("../../app");
 const connection = require("../../database/config/connection");
 const build = require("../../database/config/build");
 
-describe('Test Routes', () => {
-    beforeEach(() => build());
-    afterAll(()=> connection.end());
-    
-    test('Test POST /api/v1/user/:id/Food Route', (done) => {
+describe("Test Routes", () => {
+  beforeEach(() => build());
+  afterAll(() => connection.end());
 
-        const reqFood = {
-            food_id: 1,
-            food_category_id: 1,
-          };
+  test("Test POST /api/v1/user/:id/Food Route", (done) => {
+    const reqFood = {
+      food_id: 1,
+      food_category_id: 1,
+    };
 
-      return request(app)
-      .post('/api/v1/user/1/Food')
+    return request(app)
+      .post("/api/v1/user/1/Food")
       .send(reqFood)
       .expect(200)
-      .expect('Content-Type', /json/)
+      .expect("Content-Type", /json/)
       .end((err, res) => {
         if (err) return done(err);
-        expect(JSON.parse(res.text)).toBe({ data: null, message: "success", status: 200 });
+        expect(JSON.parse(res.text)).toBe({
+          data: null,
+          message: "success",
+          status: 200,
+        });
         done();
       });
-
   });
+});
