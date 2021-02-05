e779c6a7187794868d04663e106f6b9988c4f917
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-05 20:41:11 +0200

    modify insert food controller

--- server/controllers/insertFood/index.js
+++ server/controllers/insertFood/index.js
@@ -3,20 +3,22 @@ const Boom = require("boom");
 const { insertFoodValidation } = require("../../utils");
 
 const insertFoodController = async (req, res, next) => {
-  const { food_id, food_category_id } = req.body;
-  const user_id = req.users_id || 1;
+  const { grams } = req.body;
+  const {id:user_id, categoryId, foodId} = req.params;
   try {
     try {
       await insertFoodValidation.validateAsync({
         user_id,
-        food_id,
-        food_category_id,
-      });
+        foodId,
+        categoryId,
+        grams
+      }, { abortEarly: false } );
     } catch (err) {
-      throw Boom.badRequest("no meal selected");
+      throw Boom.badRequest(err.details.map((e) => e.message).join('\n'));
     }
-    await insertFoodQuery(user_id, food_id, food_category_id);
-    res.json({ data: null, message: "success", status: 200 });
+    const foodData = insertFoodQuery(user_id, foodId, categoryId,grams);
+    await foodData;
+    res.json({ message: "Food Successfully Added", status: 200 });
   } catch (err) {
     next(err);
   }
