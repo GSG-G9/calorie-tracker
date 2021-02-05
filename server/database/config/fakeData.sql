a1ec6bf861c62ce630564acdaf3e2732b6caba73
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-05 22:12:17 +0200

    add some comment in how to calcaute exercies calories

--- server/database/config/fakeData.sql
+++ server/database/config/fakeData.sql
@@ -16,7 +16,10 @@ INSERT INTO food(food_type_id, food_name) values (1, 'coffee');
 INSERT INTO food(food_type_id, food_name) values (1, 'apple');
 INSERT INTO food(food_type_id, food_name) values (2, 'Falafel');
 
-INSERT INTO users(first_name,last_name,email,password,weight,daily_calories_goal) VALUES('test','test','test@test.com','testpassword',116,1500);
+INSERT INTO users(first_name,last_name,email,password,weight,daily_calories_goal) VALUES('test1','test4','test1@test.com','testpassword',116,1500);
+INSERT INTO users(first_name,last_name,email,password,weight,daily_calories_goal) VALUES('test2','test4','test2@test.com','testpassword',100,1500);
+INSERT INTO users(first_name,last_name,email,password,weight,daily_calories_goal) VALUES('test3','test4','test3@test.com','testpassword',60,1500);
+INSERT INTO users(first_name,last_name,email,password,weight,daily_calories_goal) VALUES('test4','test4','test4@test.com','testpassword',150,1500);
 
 INSERT INTO food_category(name) VALUES('breakfast');
 INSERT INTO food_category(name) VALUES('lunch');
@@ -70,21 +73,15 @@ INSERT INTO exercises (exercise_name, met) VALUES
 
 
 
-INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,1,1,20);
-INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,2,1,20);
-INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,3,1,20);
-INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,4,1,20);
-INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,5,1,20);
+INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,1,1,100);
+INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,2,1,400);
+INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,3,1,500);
+INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,4,1,200);
+INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams) values(1,5,1,235);
 INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams,created_at) values(1,5,1,20,'2021-02-04 00:00:00');
 INSERT INTO UserFoodRelation(users_id,food_id,food_category_id,amount_in_grams,created_at) values(1,5,1,20,'2021-02-04 00:00:00');
-
-
-
-
-
-
 
 INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (1,1,30);
-INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (1,2,30);
-INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (1,3,30);
-INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (1,4,30);
+INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (2,2,30);
+INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (3,3,30);
+INSERT INTO UserExerciseRelation(users_id,exercises_id,exercise_duration_in_minutes) VALUES (4,4,30);
--- server/database/queries/calculateUserExercisesCalories/index.js
+++ server/database/queries/calculateUserExercisesCalories/index.js
@@ -9,11 +9,15 @@ module.exports = async (userId) =>{
 	});
 
 	/*
-	https://www.healthline.com/health/what-are-mets#examples
+	Exercise Duration = 30 min
+	User Weight = 100 kg
+	* 1000 to convert from kcal to cal
+	* 3.5/200 to convert from oxygen consumption to calories
+	calories = 1000 * 100 kg * 3.5 * MET * 30 / 200
 	*/
 	return connection.query({
 		text:
-			'SELECT SUM($1* 3.5 * exercises.met * UserExerciseRelation.exercise_duration_in_minutes / 200 ) as userExercisesCalories FROM UserExerciseRelation INNER JOIN exercises ON UserExerciseRelation.exercises_id=exercises.id WHERE UserExerciseRelation.users_id=$2 and UserExerciseRelation.created_at = current_date',
+			'SELECT SUM($1* 1000 * 3.5 * exercises.met * UserExerciseRelation.exercise_duration_in_minutes / 200 ) as userExercisesCalories FROM UserExerciseRelation INNER JOIN exercises ON UserExerciseRelation.exercises_id=exercises.id WHERE UserExerciseRelation.users_id=$2 and UserExerciseRelation.created_at = current_date',
 		values: [userWeight, userId],
 	});
 }
