c1de134eea0fd505936a830bbba8190cb4423145
imansedky5 <imans.ewaiti@gmail.com>
2021-02-06 21:46:12 +0200

    edited tests depends onthe latest changes in database

--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -1,212 +1,111 @@
-const request = require('supertest');
-const app = require('../app');
-const dbBuild = require('../database/config/build')
-const connection = require('../database/config/connection')
-
+const request = require("supertest");
+const app = require("../app");
+const dbBuild = require("../database/config/build");
+const connection = require("../database/config/connection");
+const getUserByEmail = require("../database/queries/getEmail");
 
 beforeEach(() => dbBuild());
 afterAll(() => connection.end());
 
-
-describe('sign up routes test', () => {
-  test('check bad request, status should be 400 and message '"name" is required'', (done) => {
+describe("sign up routes tests", () => {
+  test(" check if email exist, status should be 409 and (user already exist)message ", (done) => {
     return request(app)
-      .post('/api/v1/signup')
+      .post("/api/v1/signup")
+      .set({
+        "Content-Type": "application/json",
+      })
+      .send(
+        JSON.stringify({
+          lastName: "iman96",
+          email: "lina@gmail.com",
+          password: "123abc",
+          firstName: "iman sedky",
+          gender: "f",
+          minAge: "24",
+          maxAge: "40",
+          weight: "50",
+          height: "155",
+          goalWeight: "48",
+          activity: "active",
+        })
+      )
+      .expect(409)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toBe("user already exists");
+        expect(res.statusCode).toBe(409);
+
+        return done();
+      });
+  });
+
+  test("check bad request, status should be 400 and (first name is required) message ", (done) => {
+    return request(app)
+      .post("/api/v1/signup")
       .expect(400)
-          .send(JSON.stringify(
-            {
-              email: 'israssa25@gmail.com',
-              password: '123abc',
-              name: "",
-              gender: "f",
-              minAge: "254",
-              maxAge: "40",
-              weight: "510",
-              height: "115",
-              goalWeight: "48",
-              username: "iman",
-      }
-          ))
+      .send(
+        JSON.stringify({
+          email: "israssa25@gmail.com",
+          password: "123abc",
+          firstName: "",
+          gender: "f",
+          minAge: "24",
+          maxAge: "40",
+          weight: "510",
+          height: "155",
+          goalWeight: "48",
+          username: "iman",
+          activity: "active",
+        })
+      )
       .end((err, res) => {
         if (err) return done(err);
         expect(res.statusCode).toBe(400);
-       expect(res.body.message).toBe('"name" is required');
+        expect(res.body.message).toBe('"firstName" is required');
 
         done();
       });
   });
 
-
-test(' check if email exist, status should be 409 and (user already exist)message ', (done) => {
-  return request(app)
-    .post('/api/v1/signup')
-    .set({
-      'Content-Type': 'application/json',
-    })
-    .send(
-      JSON.stringify({
-          username: "iman96",
-          email: 'lina@gmail.com',
-          password: '123abc',
-          name: "iman sedky",
+  test("check successful sign-up, status should be 200 and (signed up successfully) message", (done) => {
+    return request(app)
+      .post("/api/v1/signup")
+      .set({
+        "Content-Type": "application/json",
+      })
+      .send(
+        JSON.stringify({
+          lastName: "iman96",
+          email: "imans.ewaiti@gmail.com",
+          password: "123abc",
+          firstName: "iman sedky",
           gender: "f",
           minAge: "24",
-          maxAge:"40",
+          maxAge: "40",
           weight: "50",
           height: "155",
           goalWeight: "48",
-         
-      }),
-    )
-    .expect(409)
-    .end((err, res) => {
-      if (err) return done(err);
-      expect(res.body.message).toBe('user already exists');
-      expect(res.statusCode).toBe(409);
-
-      return done();
-    });
+          activity: "active",
+        })
+      )
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.statusCode).toBe(200);
+        expect(res.body.message).toBe("signed up successfully");
+        return done();
+      });
+  });
 });
 
+describe("sign up queries tests", () => {
+  test("test getUserByEmail query, number of rows when email is exist will be 1", async () => {
+    const { rowCount } = await getUserByEmail("lina@gmail.com");
+    return expect(rowCount).toBe(1);
+  });
 
-
-test('check successful sign-up, status should be 200 and (signed up successfully) message', (done) => {
-  return request(app)
-    .post('/api/v1/signup')
-    .set({
-      'Content-Type': 'application/json',
-    })
-    .send(
-      JSON.stringify({
-        username: "iman96",
-        email: 'imans.ewaiti@gmail.com',
-        password: '123abc',
-        name: "iman sedky",
-        gender: "f",
-        minAge: "24",
-        maxAge:"40",
-        weight: "50",
-        height: "155",
-        goalWeight: "48",
-      }),
-    )
-    .expect(200)
-    .end((err, res) => {
-      if (err) return done(err);
-       expect(res.statusCode).toBe(200);
-
-      expect(res.body.message).toBe(
-        'signed up successfully',
-      );
-      return done();
-    });
+  test("test getUserByEmail query, number of rows when email is not exist will be 0", async () => {
+    const { row } = await getUserByEmail("lana@gmail.com");
+    return expect(rowCount).toBe(0);
+  });
 });
-
- });
-
-
-
-  
-
-
-
- 
-
-
-  
-
-
-
-
-
-
-
-
-// test('signup route status 200, (signed up successfully) message', (done) => {
-//   return request(app)
-//     .post('/api/v1/signup')
-//     .set({
-//       'Content-Type': 'application/json',
-//     })
-//     .send(
-//       JSON.stringify({
-//         username: "iman96",
-//         email: 'imans.ewaiti@gmail.com',
-//         password: '123abc',
-//         name: "iman sedky",
-//         gender: "f",
-//         minAge: "24",
-//         maxAge:"40",
-//         weight: "50",
-//         height: "155",
-//         goalWeight: "48",
-//       }),
-//     )
-//     .expect(200)
-//     .end((err, res) => {
-//       if (err) return done(err);
-//       expect(res.body.message).toBe(
-//         'signed up successfully',
-//       );
-//       return done();
-//     });
-// });
-
-// test('signup route, check missing name field (bad request)', (done) => {
-//   return request(app)
-//     .post('/api/v1/signup')
-//     .set({
-//       'Content-Type': 'application/json',
-//     })
-//     .send(
-//       JSON.stringify({
-//         username: "iman96",
-//         email: 'imans.ewaiti@gmail.com',
-//         password: '123abc',
-//         gender: "f",
-//         minAge: "24",
-//         maxAge:"40",
-//         weight: "50",
-//         height: "155",
-//         goalWeight: "48",
-//       }),
-//     )
-//     .expect(400)
-//     .end((err, res) => {
-//       if (err) return done(err);
-//       expect(res.body.message).toBe("\"name\" is required");
-//       return done();
-//     });
-// });
-// test('signup route, check if user exist, status 409, (user already exist)message ', (done) => {
-//   return request(app)
-//     .post('/api/v1/signup')
-//     .set({
-//       'Content-Type': 'application/json',
-//     })
-//     .send(
-//       JSON.stringify({
-//           username: "iman96",
-//           email: 'nada@gmail.com',
-//           password: '123abc',
-//           name: "iman sedky",
-//           gender: "f",
-//           minAge: "24",
-//           maxAge:"40",
-//           weight: "50",
-//           height: "155",
-//           goalWeight: "48",
-         
-//       }),
-//     )
-//     .expect(409)
-//     .end((err, res) => {
-//       if (err) return done(err);
-//       // expect(res.body.message).toBe('user already exists');
-//       expect(res.statusCode).toBe(200);
-
-//       return done();
-//     });
-// });
-
-
