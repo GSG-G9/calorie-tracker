e5188a7bea1d53da2510c7380f41196571ffc3a7
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-08 00:12:07 +0200

    modify route ,database test file

--- server/__test__/database/auth.test.js
+++ server/__test__/database/auth.test.js
@@ -16,4 +16,29 @@ describe('sign up queries tests', () => {
     const { rows } = await getUserByEmail('lana@gmail.com');
     expect(rows).toHaveLength(0);
   });
+
+  test('should return the user zein jendeya from users table', async () => {
+    const userData = [
+      {
+        id: 4,
+        activity_id: null,
+        firstname: 'zein',
+        lastname: 'jendeya',
+        email: 'zein@gmail.com',
+        password:
+          '$2b$11$5IkI6Vvo5xGqxgRw4I6uWeCJCyJyd3k7WPvnW.fgjZZQG6aSSdQLK',
+        gender: 'f',
+        height: 1.68,
+        weight: 63,
+        age: 19,
+        goalweight: null,
+        image:
+          'https://i.pinimg.com/564x/a9/f6/e3/a9f6e37a1211793bd2f45f161dc3dfbb.jpg',
+        dailycaloriesgoal: 2099.196,
+      },
+    ];
+    const { rows } = await getUserByEmail('zein@gmail.com');
+
+    return expect(rows).toEqual(userData);
+  });
 });
--- server/middlewares/errorHandle/index.js
+++ server/middlewares/errorHandle/index.js
@@ -1,12 +1,12 @@
 const clientError = (req, res) => {
-  res.status(404).send({ statusCode: 404, message: 'page not found' });
+  res.status(404).json({ statusCode: 404, message: 'page not found' });
 };
 // eslint-disable-next-line no-unused-vars
 const serverError = (err, req, res, next) => {
   const message = err.message || 'internal server error';
   const status = err.output.statusCode || 500;
 
-  res.status(status).send({ statusCode: status, message });
+  res.status(status).json({ statusCode: status, message });
 };
 
 module.exports = { clientError, serverError };
