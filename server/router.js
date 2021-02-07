6f18b3918ea078338067c723ac63c9ea5505d375
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-07 18:28:43 +0200

    resolve conflicts eslint

--- server/router.js
+++ server/router.js
@@ -1,14 +1,11 @@
-const {clientError, serverError} = require("./middlewares")
 const router = require('express').Router();
-
+const { clientError, serverError } = require('./middlewares');
 
 router.get('/hi', (req, res) => {
-    res.send('hello')
+  res.send('hello');
 });
 
 router.use(clientError);
 router.use(serverError);
 
-
-module.exports = router
-
+module.exports = router;
