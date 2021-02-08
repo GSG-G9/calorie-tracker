2883d11a7ebf824d770ea29472998596c34978e8
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-08 15:53:48 +0200

    refactoring auth middleware

--- server/middlewares/auth/index.js
+++ server/middlewares/auth/index.js
@@ -3,17 +3,12 @@ const { verifyToken } = require('../../utils');
 
 const Auth = async (req, res, next) => {
   const { token } = req.cookies;
-  if (token) {
-    try {
-      req.user = await verifyToken(token);
-      next();
-    } catch (err) {
-      throw Boom.unauthorized('You are not Authorized');
-    }
-  } else {
-    res
-      .status(401)
-      .json({ statusCode: 401, message: 'You are not registered yet' });
+  if (!token) throw Boom.unauthorized('You are not registered yet');
+  try {
+    req.user = await verifyToken(token);
+    next();
+  } catch (err) {
+    throw Boom.unauthorized('You are not Authorized');
   }
 };
 
