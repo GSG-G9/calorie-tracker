39b35d399647088256e032aa432b170840789140
zeinjendeya <z.jendeya@gmail.com>
2021-02-10 15:52:36 +0200

    changed route

--- server/__test__/index.test.js
+++ server/__test__/index.test.js
@@ -257,14 +257,14 @@ describe('authentication', () => {
     describe('food category', () => {
       it('router returns 401 if there is no user logged', async () => {
         const { status } = await request(app)
-          .get('/api/v1/user/0/category/1/food')
+          .get('/api/v1/category/1/food')
           .expect(401);
         expect(status).toBe(401);
       });
 
       it('router returns 200 if there is no food in the category', async () => {
         const { status } = await request(app)
-          .get('/api/v1/user/4/category/1/food')
+          .get('/api/v1/category/1/food')
           .set('Cookie', [`token=${tokenCategory}`])
           .expect(200);
         expect(status).toBe(200);
@@ -272,7 +272,7 @@ describe('authentication', () => {
 
       it('router returns 200 if there is food in the category', async () => {
         const { status } = await request(app)
-          .get('/api/v1/user/4/category/2/food')
+          .get('/api/v1/category/2/food')
           .set('Cookie', [`token=${tokenCategory}`])
           .expect(200);
         expect(status).toBe(200);
--- server/router.js
+++ server/router.js
@@ -14,7 +14,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 router.use(Auth);
 
-router.get('/user/:id/category/:categoryId/food', foodCategory);
+router.get('/category/:categoryId/food', foodCategory);
 router.get('/food', getFood);
 
 router.use(clientError);
