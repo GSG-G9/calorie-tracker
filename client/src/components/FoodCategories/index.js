c26152c0c1ecadd17adf52b2c88485a12bfd4f73
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-11 15:40:00 +0200

    make a folder with component name

---
+++ client/src/components/FoodCategories/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '../Button';
+
+function FoodCatagories() {
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+export default FoodCatagories;
