04a1eb78c1b3126d6bc9a802e1b374a9679c2450
imansedky5 <imans.ewaiti@gmail.com>
2021-02-12 20:46:09 +0200

    installed axios and got news data from backend

--- client/src/Pages/Home/index.js
+++ client/src/Pages/Home/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function HomePage() {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+    (async () => {
+      try {
+        const {
+          data: { data },
+        } = await axios.get('http://localhost:5000/api/v1/healthnews', {
+          cancelToken: source.token,
+        });
+        setNews(data);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+    return () => source.cancel();
+  }, []);
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
+
+// http://localhost:5000/api/v1/healthnews
