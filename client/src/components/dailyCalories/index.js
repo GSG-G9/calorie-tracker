cc7aaa680f8a24bc93d27d80832e49feed899bc5
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-14 13:20:51 +0200

    repairing daily calories component

--- client/src/components/dailyCalories/index.js
+++ client/src/components/dailyCalories/index.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Axios from 'axios';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+  },
   table: {
+    border: '1px solid black',
+
     // minWidth: 650,
-    width: '50%',
+    width: '20%',
+    // display: 'flex',
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignContent: 'center',
+  },
+  row: {
+    '&:last-child td': {
+      borderBottom: 0,
+    },
   },
 });
 
@@ -20,74 +35,69 @@ function createData(Goal, Food, Exercises, Remaining) {
   return { Goal, Food, Exercises, Remaining };
 }
 
-const rows = [createData(159, 6.0, 24, 4.0)];
-
 function DailyCalories() {
   const classes = useStyles();
 
+  const [goal, setGoal] = useState(1500);
+  const [food, setFood] = useState(0);
+  const [exercises, setExercises] = useState(0);
+  const [remaining, setRemaining] = useState(1500);
+  const [, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const getCalories = async () => {
+      try {
+        const response = await Axios.get('/api/v1/user/calories');
+
+        setGoal(response.data.data.userCalories);
+
+        setFood(response.data.data.userFoodCalories);
+
+        setExercises(response.data.data.userExercisesCalories);
+
+        return setRemaining(response.data.data.remainingCalories);
+      } catch (err) {
+        if (err.response.data.message) {
+          return setErrorMessage(err.response.data.message);
+        }
+        return setErrorMessage('something wrong');
+      }
+    };
+    getCalories();
+  }, []);
+
+  const rows = [createData(goal, food, exercises, remaining)];
+
   return (
     <>
-      <TableContainer className={classes.table}>
-        <Table aria-label="simple table">
+      <TableContainer className={classes.container}>
+        <Table aria-label="simple table" className={classes.table}>
           <TableBody>
             {rows.map((row) => (
-              <TableRow>
+              <TableRow className={classes.row}>
                 <TableCell align="center">{row.Goal}</TableCell>
+                <TableCell align="center"> - </TableCell>
                 <TableCell align="center">{row.Food}</TableCell>
+                <TableCell align="center"> + </TableCell>
                 <TableCell align="center">{row.Exercises}</TableCell>
+                <TableCell align="center"> = </TableCell>
                 <TableCell align="center">{row.Remaining}</TableCell>
               </TableRow>
             ))}
           </TableBody>
 
-          <TableRow>
+          <TableRow className={classes.row}>
             <TableCell align="center">Goal</TableCell>
+            <TableCell align="center" />
             <TableCell align="center">Food</TableCell>
+            <TableCell align="center" />
             <TableCell align="center">Exercises</TableCell>
+            <TableCell align="center" />
             <TableCell align="center">Remaining</TableCell>
+            <TableCell align="center" />
           </TableRow>
         </Table>
       </TableContainer>
-      <div style={{ width: '40%' }}>
-        <Box
-          component="span"
-          display="block"
-          p={1}
-          m={1}
-          // bgcolor="background.paper"
-        >
-          1500 - 0 + 0 = 1500
-        </Box>
-        <Box
-          component="span"
-          display="block"
-          p={1}
-          m={1}
-          bgcolor="background.paper"
-        >
-          block
-        </Box>
-      </div>
-      <div style={{ width: '100%' }}>
-        <Box
-          component="div"
-          display="inline"
-          p={1}
-          m={1}
-          bgcolor="background.paper"
-        >
-          inline
-        </Box>
-        <Box
-          component="div"
-          display="inline"
-          p={1}
-          m={1}
-          bgcolor="background.paper"
-        >
-          inline
-        </Box>
-      </div>
     </>
   );
 }
