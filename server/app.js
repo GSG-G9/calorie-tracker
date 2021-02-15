d566845dedc064decc66c8c369891431668d1b51
hassanelnajjar <hassansamielnajjar@gmail.com>
2021-02-15 10:05:27 +0200

    fix app.js - move the clientError at bottom of the file

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 const cookieParser = require('cookie-parser');
 
 const router = require('./router');
-const { clientError } = require('./middlewares');
+const { clientError, serverError } = require('./middlewares');
 
 const app = express();
 
@@ -22,13 +22,12 @@ const middlewares = [
 app.use(middlewares);
 app.use('/api/v1/', router);
 
-app.use(clientError);
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(join(__dirname, '..', 'client', 'build')));
   app.all('*', (req, res) => {
     res.send(join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
-
+app.use(clientError);
+app.use(serverError);
 module.exports = app;
