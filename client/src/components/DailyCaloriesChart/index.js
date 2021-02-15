5f579b53d2a38b3da4c7258821003a8677ce78da
Khamis Hesham <khamis1997.kh@gmail.com>
2021-02-15 04:59:54 +0200

    create daily calories chart component

--- client/src/components/DailyCaloriesChart/index.js
+++ client/src/components/DailyCaloriesChart/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-function DailyCaloriesChart() {
-  return <div />;
+import DoughnutChart from '../DoughnutChart';
+
+const { number } = PropTypes;
+const useStyles = makeStyles({
+  chart: {
+    '@media (min-device-width: 900px)': {
+      width: '10',
+      height: '2',
+    },
+  },
+});
+
+function DailyCaloriesChart({ goal, remaining }) {
+  const classes = useStyles();
+
+  const [section, setSection] = useState([goal, goal - remaining]); // 200, 200 - 100
+
+  console.log(section);
+  const [sectionBackground, setSectionBackground] = useState(['blue', 'red']);
+  const [width, setWidth] = useState([15]);
+  const [height, setHeight] = useState([5]);
+
+  return (
+    <>
+      {console.log(goal)}
+      <DoughnutChart
+        section={section}
+        sectionBackground={sectionBackground}
+        width={width}
+        height={height}
+        className={classes.chart}
+      />
+    </>
+  );
 }
+
+DailyCaloriesChart.propTypes = {
+  goal: number.isRequired,
+  remaining: number.isRequired,
+};
 
 export default DailyCaloriesChart;
